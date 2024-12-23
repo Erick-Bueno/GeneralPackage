2251901df086c9b6c45af283626ebfc5c0682fae
Erick bueno <erickjb93@gmail.com>
2024-12-23 16:59:33 -0300

    refactor: add random data in tests

--- test/utils/string_formatter/string_formatter_test.dart
+++ test/utils/string_formatter/string_formatter_test.dart
@@ -1,37 +1,42 @@
+import 'package:faker/faker.dart';
 import 'package:pacotasso/utils/string_formatter/string_formatter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('StringFormatter deve capitalizar a primeira letra da string', () {
-    test('capitalizeFirstLetter', () {
-      const input = 'flutter';
-      const expectedOutput = 'Flutter';
+  group('StringFormatter', () {
+    test('capitalizeFirstLetter deve capitalizar a primeira letra da string',
+        () {
+      var input = faker.person.firstName();
+      var expectedOutput = input[0].toUpperCase() + input.substring(1);
 
       final result = input.capitalizeFirstLetter();
 
       expect(result, expectedOutput);
     });
 
-    test('StringFormatter deve disparar erro caso a string esteja vazia', () {
+    test('capitalizeFirstLetter deve disparar erro caso a string esteja vazia',
+        () {
       const input = '';
-      expect(() => input.capitalizeFirstLetter(),
-          throwsA(isA<RangeError>()));
+      expect(() => input.capitalizeFirstLetter(), throwsA(isA<RangeError>()));
     });
 
     test('getFirstName deve retornar o primeiro nome', () {
-      const input = 'John Doe';
-      const expectedOutput = 'John';
+      var input = faker.person.name();
+      var expectedOutput = input.split(' ')[0];
       final result = input.getFirstName();
 
       expect(result, expectedOutput);
     });
     test('getFirstName deve retornar o  nome inteiro caso não tenha espaços',
         () {
-      const input = 'JohnDoe';
-      const expectedOutput = 'JohnDoe';
-      final result = input.getFirstName();
+      var firstName = faker.person.firstName();
+      var lastName = faker.person.lastName();
 
-      expect(result, expectedOutput);
+      var nameWithoutSpaces = firstName + lastName;
+
+      var result = nameWithoutSpaces.getFirstName();
+
+      expect(result, nameWithoutSpaces);
     });
   });
 }
